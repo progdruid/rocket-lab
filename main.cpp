49ff12b492fbce77285937c82168cc307d8d1cd4
progdruid <progdruid@gmail.com>
2023-09-22 22:20:10 +0100

    glfw linked

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
-#define BX_CONFIG_DEBUG 1;
+#define BX_CONFIG_DEBUG 0;
+
+#include <iostream>
 
 #include "bx/bx.h"
 #include "bimg/bimg.h"
 #include "bgfx/bgfx.h"
+#include "glfw/glfw3.h"
 
 int main()
 {
+	bx::debugPrintf("test");
+	GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+	glfwCreateWindow(100, 100, "Title", NULL, NULL);
+	int m;
+	std::cin >> m;
 	return 0;
 }
