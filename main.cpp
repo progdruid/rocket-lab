29300f9ac9accd6d19acba39487ce3c329706bd0
progdruid <progdruid@gmail.com>
2023-09-26 01:11:11 +0100

    update

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #define LAB_WIDTH 800
 #define LAB_HEIGHT 600
 
-#define BX_CONFIG_DEBUG 1;
-#define _ITERATOR_DEBUG_LEVEL 0
-
 #include <iostream>
 
+#define _ITERATOR_DEBUG_LEVEL 0
+#define BX_CONFIG_DEBUG 1;
 #include "bx/bx.h"
 #include "bimg/bimg.h"
 #include "bgfx/bgfx.h"
 #include "bgfx/platform.h"
 
 #define GLFW_EXPOSE_NATIVE_WIN32
+#include <Windows.h>
 #include "glfw/glfw3.h"
 #include "glfw/glfw3native.h"
 
@@ -21,7 +21,6 @@ int main()
 		return -1;
 
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
-	//glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 	
 	GLFWwindow* window = glfwCreateWindow(LAB_WIDTH, LAB_HEIGHT, "Rocket Lab", NULL, NULL);
 	if (!window)
@@ -33,20 +32,27 @@ int main()
 	glfwMakeContextCurrent(window);
 
 	bgfx::PlatformData pd;
-	//pd.context = window;
+	pd.type = bgfx::NativeWindowHandleType::Count;
+	pd.context = GetDC(glfwGetWin32Window(window));
 	pd.nwh = glfwGetWin32Window(window);
-	
+
 	bgfx::Init init;
 	init.platformData = pd;
-	init.type = bgfx::RendererType::OpenGL;
+	init.type = bgfx::RendererType::Direct3D12;
 	bgfx::init(init);
+
+	int windowWidth, windowHeight;
 
 	while (!glfwWindowShouldClose(window))
 	{
+		//glfwGetWindowSize(window, &windowWidth, &windowHeight);
+		glfwGetFramebufferSize(window, &windowWidth, &windowHeight);
 		glfwPollEvents();
 
+		//std::cout << windowWidth << " " << windowHeight << std::endl;
 		bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x405030ff, 1.0f, 0);
-		bgfx::setViewRect(0, 0, 0, uint16_t(LAB_WIDTH), uint16_t(LAB_HEIGHT));
+		bgfx::setViewRect(0, 0, 0, uint16_t(windowWidth), uint16_t(windowHeight));
+		//bgfx::setViewRect(0, 0, 0, bgfx::BackbufferRatio::Equal);
 		bgfx::touch(0);
 
 		bgfx::frame();
